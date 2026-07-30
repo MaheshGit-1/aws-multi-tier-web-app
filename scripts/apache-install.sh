bedf74245278dcda05bc76265c1fb5f481bab53e
MaheshGit-1 <168809235+MaheshGit-1@users.noreply.github.com>
2026-07-31 02:09:18 +0530

    Add Apache installation script

--- scripts/apache-install.sh
+++ scripts/apache-install.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+echo "Updating packages..."
+
+sudo yum update -y
+
+echo "Installing Apache..."
+
+sudo yum install httpd -y
+
+echo "Starting Apache..."
+
+sudo systemctl start httpd
+
+echo "Enabling Apache..."
+
+sudo systemctl enable httpd
+
+echo "<h1>AWS Multi-Tier Web Application</h1>" | sudo tee /var/www/html/index.html
+
+echo "Deployment Complete."
